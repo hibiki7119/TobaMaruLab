8f6fb38cff4bc715497ecd751fa50b15cc60b5ed
16423 <16423@toba.kosen-ac.jp>
2018-08-03 14:52:54 +0900

    変更４

---
+++ Assets/ScoreText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour {
+	//点数を格納する変数
+	    public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<Text>().text = "点数" + score.toString() + "点";
+	}
+}
---
+++ Assets/Script00.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script00 : MonoBehaviour {
+	//押された回数を格納する変数
+	    private int counter = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if　　（Input.GetButtonDown && ) {
+            counter++;  //****追加
+						if (counter <= 3) {//****追加
+                Instantiate(this.ballPrefab);
+            }
+	}
+}
